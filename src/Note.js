3d0adff6c285df70d61939738101e8417b98abb1
Calente Cardwell <nxmxgoldxx@live.com>
2022-02-25 18:54:25 -0800

    new onChance, props onType from App & Note ...

--- src/Note.js
+++ src/Note.js
@@ -1,6 +1,21 @@
 import React from "react";
 
-const Note = (props) => (
+const Note = (props) => { 
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props. note.id;
+    props.onType(editMeId, "title", updatedValue);
+  }
+  
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "description", updatedValue);
+  };
+ 
+  
+  
+  return (
   <li className="note">
     <input
       type="text"
@@ -15,6 +30,6 @@ const Note = (props) => (
     />
     <span className="note__delete">X</span>
   </li>
-);
-
+  );
+};
 export default Note;
--- src/NotesList.js
+++ src/NotesList.js
@@ -4,7 +4,8 @@ import Note from "./Note.js";
 const NotesList = (props) => {
   const keepSearchMatches = (note) => note.DoesMatchSearch;
   const searchMatches = props.notes.filter(keepSearchMatches);
-  const renderNote = (note) => <Note note={note} key={note.id} />;
+
+  const renderNote = (note) => <Note onType={props.onType} note={note} key={note.id} />;
   const noteElements = props.notes.map(renderNote);
   return <ul className="notes-list">{noteElements}</ul>;
 };
